bcbc8b71964bf832a1caf85535fbb99150781334
Brahm Yachnin <brahm.yachnin@rutgers.edu>
2022-09-10 11:35:55 -0400

    Switch on Des gens et des lieux exhibition

--- globals.php
+++ globals.php
@@ -4,16 +4,16 @@
 	$announcement_text =
 		'Please join Lina and the other Galerie Old Chelsea artists for a group exhibition, "The Wonders of Winter," from January 27 to February 13.  For more information, visit <a href="https://www.galerieoldchelsea.ca/exhibitions" class="text-center" style="display: inline">Galerie Old Chelsea</a>';
 	$hide_announce_img = false; // Include a poster type image?
-	$announcement_img = "img/Wonders_of_Winter_2022_Poster.jpg"; // Path to poster type image
+	$announcement_img = "img/Des_gens_et_des_lieux_2022_Poster.jpg"; // Path to poster type image
 
 	//Exhibition page related flags
-	$exhibition = false;	//Use special exhibition text
-	$exhibition_paintings = true;	//Include the paintings on the exhibition page.
-	$ex_title = "The Wonders of Winter";		//The title of the Exhibitions page.
+	$exhibition = true;	//Use special exhibition text
+	$exhibition_paintings = false;	//Include the paintings on the exhibition page.
+	$ex_title = "Des gens et des lieux";		//The title of the Exhibitions page.
 	$ex_banner_style = "bg-First-Snow-II";	//The background picture style for the exhibitions.  Must update CSS!
-	// $upcoming_text =
-	// 	'My most recent show, <i>Through the Trees</i>, took place from October 16 - November 4 at <a href="http://www.galerieoldchelsea.ca" class="text-center" style="display: inline">Galerie Old Chelsea</a>.  A few paintings from the show are still available at the Galerie.<br/><br/>The Canadian landscape is an inspiration. Our home in Quebec is in an old growth forest which I love. It has provided endless possibilities for colourful, energetic paintings. This show is focused on our relationship with trees. Through a semi-abstract approach, integrating the elements of composition and colour, I hope to capture the extraordinary. The landscape is always changing. Painting the trees first gives them primary importance. The background brings us into the scene and gives us a place to be. Enjoy looking “Through The Trees” and my new works in watercolour!<br/><br/>A recent <a href="https://www.lowdownonline.com/" class="text-center" style="display: inline"><i>The Low Down</i></a> article featured my painting, <i>Through the Trees</i>, in an article on supporting local artists and galleries.  Please <a href="http://origin.misc.pagesuite.com/pdfdownload/ec015f5c-cc01-44c0-8e44-2862e885d4d4.pdf" class="text-center" style="display: inline">check it out</a>!';
-	$upcoming_text = $announcement_text;
+	$upcoming_text =
+	'My upcoming show, <i>Des gens et des lieux</i>, will take place from October 7 - 26 at <a href="http://www.galerieoldchelsea.ca" class="text-center" style="display: inline">Galerie Old Chelsea</a>.<br/><br/><i>Des gens et des lieux</i> is a show about memorable places. Some are far. Some are close to home, but always close to the heart. This work crystallizes memories into images. Travels, hikes and outdoor activities influence my work. The use of colour is slightly exaggerated to draw attention to our beautiful surroundings. I work both in oils and watercolour. I enjoy the two mediums because they allow for a very different expression of my experiences.';
+	// $upcoming_text = $announcement_text;
 
 
 	//Standard exhibition blurb.
